b9797e6cd764be4251cb8d455c870e43dbb0502b
Lucas Frey <lucas.frey@ptml.com>
2025-02-17 22:33:25 +1300

    whatever typescript

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -71,7 +71,7 @@ export const adaptOpenGraphImages = async (
           };
         }
 
-        let _image;
+        let _image: ImageMetadata | { src: string; width?: number; height?: number } | undefined;
 
         if (
           typeof resolvedImage === 'string' &&
